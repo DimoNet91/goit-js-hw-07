47756b25e67c386939ae0254f3575bbd814a9ca1
Dmitriy <malish3250@gmail.com>
2024-04-29 09:22:45 +0300

    upd task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,45 +1,34 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, "0")}`;
-}
+const boxesContainer = document.getElementById('boxes');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const inputNumber = document.querySelector('input[type="number"]');
 
-function createBoxes(amount) {
-  const boxesContainer = document.getElementById("boxes");
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+};
 
-  boxesContainer.innerHTML = "";
-
+const createBoxes = amount => {
+  let boxesHTML = '';
   let size = 30;
 
   for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    boxesHTML += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}"></div>`;
     size += 10;
   }
-}
 
-function destroyBoxes() {
-  const boxesContainer = document.getElementById("boxes");
-  boxesContainer.innerHTML = "";
-}
+  boxesContainer.innerHTML = boxesHTML;
+};
 
-const createButton = document.querySelector("[data-create]");
-const destroyButton = document.querySelector("[data-destroy]");
-const input = document.querySelector('input[type="number"]');
+const destroyBoxes = () => {
+  boxesContainer.innerHTML = '';
+};
 
-createButton.addEventListener("click", () => {
-  const amount = parseInt(input.value);
+createButton.addEventListener('click', () => {
+  const amount = parseInt(inputNumber.value);
   if (amount >= 1 && amount <= 100) {
     createBoxes(amount);
-    input.value = "";
-  } else {
-    alert("Please enter a number between 1 and 100.");
+    inputNumber.value = '';
   }
 });
 
-destroyButton.addEventListener("click", () => {
-  destroyBoxes();
-});
+destroyButton.addEventListener('click', destroyBoxes);
